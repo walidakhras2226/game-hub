c9c56347a32938a36227953a4a19d8f202b9ed55
Walid Akhras <walid.akhras2004@gmail.com>
2024-07-15 21:35:58 +0400

    Fetching Platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { Platform } from "./useGames";
+import { ResponseProperties } from "./useData";
 import platforms from "../data/platforms";
 
-const usePlatforms = () => ({ data: platforms, error: null });
+const usePlatforms = () => useQuery({
+    queryKey: ['platforms'],
+    queryFn: () => apiClient.get<ResponseProperties<Platform>>('/platforms/lists/parents').then(res => res.data),
+    staleTime: 24 * 60 * 60 * 1_000, // 24 hours
+    initialData: { count: platforms.length, results: platforms },
 
+})
 export default usePlatforms;
