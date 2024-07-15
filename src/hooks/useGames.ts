7be61327ad3c8f0765adf0173ce30dcd2828641f
Walid Akhras <walid.akhras2004@gmail.com>
2024-07-15 23:52:56 +0400

    Implemented infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -28,7 +28,8 @@ const useGames = (gameQuery: GameQuery) => useInfiniteQuery<ResponseProperties<G
   initialPageParam: 1,
   getNextPageParam: (lastPage, allPages) => {
     return lastPage.next ? allPages.length + 1 : undefined;
-  }
+  },
+  staleTime: 24 * 60 * 60 * 1_000,
 })
 
 export default useGames;
