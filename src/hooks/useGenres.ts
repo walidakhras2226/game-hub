1f92d7aa85b83bac18efc7f808dd832dd1e19ca1
Walid Akhras <walid.akhras2004@gmail.com>
2024-07-19 22:30:39 +0400

    Moved interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,22 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import ms from 'ms';
+import ms from "ms";
 import APIClient from "../services/api-client";
 import genres from "../data/genres";
+import { Genre } from "../entities/Genre";
 
-const apiClient = new APIClient<Genre>('/genres');
+const apiClient = new APIClient<Genre>("/genres");
 
-export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
-}
-
-
-const useGenres = () => useQuery({
-    queryKey: ['genres'],
+const useGenres = () =>
+  useQuery({
+    queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: ms("24h"),
     initialData: genres,
-});
+  });
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,20 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APIClient from "../services/api-client";
 import platforms from "../data/platforms";
+import { Platform } from "../entities/Platform";
 
-const apiClient = new APIClient<Platform>('/platforms/lists/parents');
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
-export interface Platform {
-    id: number,
-    name: string,
-    slug: string,
-}
-
-const usePlatforms = () => useQuery({
-    queryKey: ['platforms'],
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: ms("24h"),
     initialData: platforms,
-
-})
+  });
 export default usePlatforms;
